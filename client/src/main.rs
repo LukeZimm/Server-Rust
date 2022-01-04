0550f6993bf1bf170f55b7be629426e99c06f320
LukeZimm <lukezimm2014@gmail.com>
2022-01-03 22:43:38 -0500

    oop with structs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,65 +1,75 @@
+use std::io::Error;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 
 const HEADER_SIZE: usize = 8;
 
-fn main() {
-    const PORT: &str = "3333";
-    match TcpStream::connect(String::from("127.0.0.1:") + PORT) {
-        Ok(mut stream) => {
-            println!("Successfully connected to server in port: {}", PORT);
-            loop {
-                let mut msg = String::new();
-                println!("Message: ");
-                let _ = std::io::stdin().read_line(&mut msg).unwrap();
-                let msg = msg.as_bytes();
-                send(&mut stream, msg);
-                println!("Sent message, awaiting reply...");
-                let mut data = [0 as u8; HEADER_SIZE]; // read header
-                match stream.read_exact(&mut data) {
+pub struct Server {
+    stream: TcpStream,
+    header_size: usize,
+}
+impl Server {
+    pub fn new(addr: &str, port: &str) -> Result<Server, Error> {
+        match TcpStream::connect(String::from(addr) + ":" + port) {
+            Ok(stream) => {
+                println!("Successfully connected to server with port: {}", port);
+                Ok(Server {
+                    stream,
+                    header_size: 8,
+                })
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+        let mut data = vec![0 as u8; self.header_size];
+        match self.stream.read_exact(&mut data) {
+            Ok(_) => {
+                let len = from_utf8(&data).unwrap().parse::<usize>().unwrap();
+                let mut data = vec![0 as u8; len]; // read header
+                match self.stream.read_exact(&mut data) {
                     Ok(_) => {
-                        let len = from_utf8(&data).unwrap().parse::<usize>().unwrap();
-                        let mut data = vec![0 as u8; len]; // read header
-                        match stream.read_exact(&mut data) {
-                            Ok(_) => {
-                                println!("Reply: {}", from_utf8(&data).unwrap());
-                            }
-                            Err(e) => {
-                                println!("Failed to recieve data: {}", e);
-                            }
-                        }
+                        // println!("Reply: {}", from_utf8(&data).unwrap());
+                        Ok(data)
                     }
-                    Err(e) => {
-                        println!("Failed to recieve data: {}", e);
-                    }
+                    Err(e) => Err(e),
                 }
             }
+            Err(e) => Err(e),
         }
-        Err(e) => {
-            println!("Failed to connect: {}", e);
-        }
+    }
+    pub fn send(&mut self, msg: &[u8]) -> Result<usize, Error> {
+        self.stream
+            .write(pad_front(&msg.len().to_string(), self.header_size, "0").as_bytes())
+            .unwrap();
+        self.stream.write(&msg)
+    }
+}
+
+fn main() {
+    let mut server = Server::new("127.0.0.1", "3333").unwrap();
+    loop {
+        let mut msg = String::new();
+        println!("Message: ");
+        let _ = std::io::stdin().read_line(&mut msg).unwrap();
+        let msg = msg.strip_suffix("\n").unwrap().as_bytes();
+        server.send(msg).unwrap();
+        println!("{}", from_utf8(&server.receive().unwrap()).unwrap());
     }
     println!("Terminated.");
 }
 
-fn send(stream: &mut TcpStream, msg: &[u8]) {
-    stream
-        .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
-        .unwrap();
-    stream.write(msg).unwrap();
-}
-
 fn pad_front(msg: &str, len: usize, ch: &str) -> String {
     let mut out = String::new();
-    for i in msg.len()..len {
+    for _i in msg.len()..len {
         out += ch;
     }
     out + msg
 }
 fn pad_back(msg: &str, len: usize, ch: &str) -> String {
     let mut out = String::from(msg);
-    for i in msg.len()..len {
+    for _i in msg.len()..len {
         out += ch;
     }
     out
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,82 +1,105 @@
+use std::io::Error;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::str::from_utf8;
 use std::thread;
 
-const HEADER_SIZE: usize = 8;
-
-fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; HEADER_SIZE];
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            // stream.write(&data[0..size]).unwrap();
-            let len = from_utf8(&data).unwrap().parse::<usize>().unwrap();
-            let mut data = vec![0 as u8; len]; // read header
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    let msg = from_utf8(&data).unwrap();
-                    println!("Recieved: {}", msg);
-                    let msg = msg.as_bytes();
-                    send(&mut stream, msg);
+pub struct Server {
+    listener: TcpListener,
+    header_size: usize,
+    port: String,
+}
+impl Server {
+    pub fn new(addr: &str, port: &str) -> Server {
+        let listener = TcpListener::bind(String::from(addr) + ":" + port).unwrap();
+        Server {
+            listener,
+            header_size: 8,
+            port: String::from(port),
+        }
+    }
+    pub fn listen(&mut self) {
+        // accept connection and process them, spawning a new thread for each one
+        println!("Server listening on port {}", self.port);
+        for stream in self.listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("New connection: {}", stream.peer_addr().unwrap());
+                    thread::spawn(move || {
+                        // connection succeeded
+                        handle_client(stream)
+                    });
                 }
                 Err(e) => {
-                    println!("Failed to recieve data: {}", e);
+                    println!("Error: {}", e);
+                    // connection failed
                 }
             }
+        }
+    }
+}
+
+pub struct Client {
+    stream: TcpStream,
+    header_size: usize,
+}
+impl Client {
+    pub fn new(stream: TcpStream) -> Client {
+        Client {
+            stream,
+            header_size: 8,
+        }
+    }
+    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+        let mut head = vec![0 as u8; self.header_size];
+        match self.stream.read(&mut head) {
+            Ok(_) => {
+                let len = from_utf8(&head).unwrap().parse::<usize>().unwrap();
+                let mut data = vec![0 as u8; len];
+                match self.stream.read_exact(&mut data) {
+                    Ok(_) => Ok(data),
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn send(&mut self, msg: &[u8]) -> Result<usize, Error> {
+        self.stream
+            .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
+            .unwrap();
+        self.stream.write(msg)
+    }
+}
+
+const HEADER_SIZE: usize = 8;
+
+fn handle_client(stream: TcpStream) {
+    let mut client = Client::new(stream);
+    while match client.receive() {
+        Ok(data) => {
+            client.send(&data).unwrap();
             true
         }
-        Err(_) => {
-            println!(
-                "An error has occured, terminating connection with {}",
-                stream.peer_addr().unwrap()
-            );
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
+        Err(e) => false,
     } {}
 }
 
 fn main() {
-    const PORT: &str = "3333";
-    let listener = TcpListener::bind(String::from("127.0.0.1:") + PORT).unwrap();
-    // accept connection and process them, spawning a new thread for each one
-    println!("Server listening on PORT {}", PORT);
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                // connection failed
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
-}
-
-fn send(stream: &mut TcpStream, msg: &[u8]) {
-    stream
-        .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
-        .unwrap();
-    stream.write(msg).unwrap();
+    let mut server = Server::new("127.0.0.1", "3333");
+    server.listen();
 }
 
 fn pad_front(msg: &str, len: usize, ch: &str) -> String {
     let mut out = String::new();
-    for i in msg.len()..len {
+    for _i in msg.len()..len {
         out += ch;
     }
     out + msg
 }
 fn pad_back(msg: &str, len: usize, ch: &str) -> String {
     let mut out = String::from(msg);
-    for i in msg.len()..len {
+    for _i in msg.len()..len {
         out += ch;
     }
     out
