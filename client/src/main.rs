a15a8833e9a1f6519e2d1a8f94e7f1687e68d1b8
LukeZimm <lukezimm2014@gmail.com>
2022-01-03 20:32:31 -0500

    basic header

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,26 +2,37 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 
+const HEADER_SIZE: usize = 8;
+
 fn main() {
-    let port = "3333";
-    match TcpStream::connect(String::from("127.0.0.1:") + port) {
+    const PORT: &str = "3333";
+    match TcpStream::connect(String::from("127.0.0.1:") + PORT) {
         Ok(mut stream) => {
-            println!("Successfully connected to server in port: {}", port);
-            let msg = b"Hello!";
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
-            let mut data = [0 as u8; 6]; // 'Hello!' buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text)
+            println!("Successfully connected to server in port: {}", PORT);
+            loop {
+                let mut msg = String::new();
+                println!("Message: ");
+                let _ = std::io::stdin().read_line(&mut msg).unwrap();
+                let msg = msg.as_bytes();
+                send(&mut stream, msg);
+                println!("Sent message, awaiting reply...");
+                let mut data = [0 as u8; HEADER_SIZE]; // read header
+                match stream.read_exact(&mut data) {
+                    Ok(_) => {
+                        let len = from_utf8(&data).unwrap().parse::<usize>().unwrap();
+                        let mut data = vec![0 as u8; len]; // read header
+                        match stream.read_exact(&mut data) {
+                            Ok(_) => {
+                                println!("Reply: {}", from_utf8(&data).unwrap());
+                            }
+                            Err(e) => {
+                                println!("Failed to recieve data: {}", e);
+                            }
+                        }
                     }
-                }
-                Err(e) => {
-                    println!("Failed to recieve data: {}", e);
+                    Err(e) => {
+                        println!("Failed to recieve data: {}", e);
+                    }
                 }
             }
         }
@@ -30,4 +41,26 @@ fn main() {
         }
     }
     println!("Terminated.");
+}
+
+fn send(stream: &mut TcpStream, msg: &[u8]) {
+    stream
+        .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
+        .unwrap();
+    stream.write(msg).unwrap();
+}
+
+fn pad_front(msg: &str, len: usize, ch: &str) -> String {
+    let mut out = String::new();
+    for i in msg.len()..len {
+        out += ch;
+    }
+    out + msg
+}
+fn pad_back(msg: &str, len: usize, ch: &str) -> String {
+    let mut out = String::from(msg);
+    for i in msg.len()..len {
+        out += ch;
+    }
+    out
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,28 @@
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str::from_utf8;
 use std::thread;
 
+const HEADER_SIZE: usize = 8;
+
 fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50]; //50 byte buffer
+    let mut data = [0 as u8; HEADER_SIZE];
     while match stream.read(&mut data) {
         Ok(size) => {
-            // echo everything
-            stream.write(&data[0..size]).unwrap();
+            // stream.write(&data[0..size]).unwrap();
+            let len = from_utf8(&data).unwrap().parse::<usize>().unwrap();
+            let mut data = vec![0 as u8; len]; // read header
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    let msg = from_utf8(&data).unwrap();
+                    println!("Recieved: {}", msg);
+                    let msg = msg.as_bytes();
+                    send(&mut stream, msg);
+                }
+                Err(e) => {
+                    println!("Failed to recieve data: {}", e);
+                }
+            }
             true
         }
         Err(_) => {
@@ -22,10 +37,10 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() {
-    let port = "3333";
-    let listener = TcpListener::bind(String::from("127.0.0.1:") + port).unwrap();
+    const PORT: &str = "3333";
+    let listener = TcpListener::bind(String::from("127.0.0.1:") + PORT).unwrap();
     // accept connection and process them, spawning a new thread for each one
-    println!("Server listening on port {}", port);
+    println!("Server listening on PORT {}", PORT);
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -43,4 +58,26 @@ fn main() {
     }
     // close the socket server
     drop(listener);
+}
+
+fn send(stream: &mut TcpStream, msg: &[u8]) {
+    stream
+        .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
+        .unwrap();
+    stream.write(msg).unwrap();
+}
+
+fn pad_front(msg: &str, len: usize, ch: &str) -> String {
+    let mut out = String::new();
+    for i in msg.len()..len {
+        out += ch;
+    }
+    out + msg
+}
+fn pad_back(msg: &str, len: usize, ch: &str) -> String {
+    let mut out = String::from(msg);
+    for i in msg.len()..len {
+        out += ch;
+    }
+    out
 }
