1454c68ab065653896fcefdda1e98a1d50b05fc7
LukeZimm <lukezimm2014@gmail.com>
2022-01-05 16:41:29 -0500

    slightly more advanced header

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,17 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
 use std::io::Error;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
+use std::time::{Duration, Instant};
 
 const HEADER_SIZE: usize = 8;
 
 pub struct Server {
     stream: TcpStream,
     header_size: usize,
+    handshake_data: Vec<(String, usize)>,
 }
 impl Server {
     pub fn new(addr: &str, port: &str) -> Result<Server, Error> {
@@ -17,12 +21,13 @@ impl Server {
                 Ok(Server {
                     stream,
                     header_size: 8,
+                    handshake_data: Vec::new(),
                 })
             }
             Err(e) => Err(e),
         }
     }
-    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+    pub fn receive_basic(&mut self) -> Result<Vec<u8>, Error> {
         let mut data = vec![0 as u8; self.header_size];
         match self.stream.read_exact(&mut data) {
             Ok(_) => {
@@ -39,24 +44,113 @@ impl Server {
             Err(e) => Err(e),
         }
     }
-    pub fn send(&mut self, msg: &[u8]) -> Result<usize, Error> {
+    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+        let mut data = vec![0 as u8; self.header_size];
+        match self.stream.read_exact(&mut data) {
+            Ok(_) => {
+                // println!("{:?}", data);
+                let mut head: HashMap<String, &[u8]> = HashMap::new();
+                let mut current: usize = 0;
+                for i in &self.handshake_data {
+                    head.insert(String::from(&i.0), &data[current..current + i.1]);
+                    current += i.1;
+                }
+                // println!("{:?}", head);
+                let len = head.get("len").copied().unwrap();
+                let len = usize::from_le_bytes(pop(len));
+                let pkg_type = from_utf8(head.get("pkg_type").unwrap()).unwrap();
+                let mut data = vec![0 as u8; len]; // read header
+                match self.stream.read_exact(&mut data) {
+                    Ok(_) => {
+                        match pkg_type {
+                            "rqst" => {
+                                let request = from_utf8(&data).unwrap();
+                                match request {
+                                    "name" => {
+                                        let mut msg = String::new();
+                                        println!("Name: ");
+                                        let _ = std::io::stdin().read_line(&mut msg).unwrap();
+                                        let msg = msg.strip_suffix("\n").unwrap().as_bytes();
+                                        self.send(b"data", msg).unwrap();
+                                    }
+                                    &_ => {}
+                                }
+                            }
+                            "ping" => {}
+                            "data" => {}
+                            &_ => {}
+                        }
+                        // println!("Reply: {}", from_utf8(&data).unwrap());
+                        Ok(data)
+                    }
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn send_basic(&mut self, msg: &[u8]) -> Result<usize, Error> {
         self.stream
-            .write(pad_front(&msg.len().to_string(), self.header_size, "0").as_bytes())
+            .write(pad_front(&msg.len().to_string(), 8, "0").as_bytes()) // change hard coded 8
             .unwrap();
         self.stream.write(&msg)
+    }
+    pub fn send(&mut self, pkg_type: &[u8], msg: &[u8]) -> Result<usize, Error> {
+        let len = msg.len().to_le_bytes();
+        let mut head = Vec::from(len);
+        head.extend_from_slice(pkg_type);
+        self.stream.write(&head).unwrap();
+        self.stream.write(msg)
+    }
+    pub fn handshake(&mut self) {
+        println!("ping: {}ms", self.ping_basic().as_millis());
+        let data = self.receive_basic().unwrap();
+        let data = from_utf8(&data).unwrap();
+        // println!("{}", data);
+        let data = data.split(",");
+        let mut handshake_data: Vec<(String, usize)> = Vec::new();
+        let mut header_size: usize = 0;
+        for i in data {
+            let j: Vec<&str> = i.split(":").collect();
+            let k = j[1].parse::<usize>().unwrap();
+            handshake_data.push((String::from(j[0]), k));
+            header_size += k;
+        }
+        self.handshake_data = handshake_data;
+        self.header_size = header_size;
+    }
+    pub fn ping_basic(&mut self) -> Duration {
+        let t1 = Instant::now();
+        self.send_basic(b"ping").unwrap();
+        self.receive_basic().unwrap();
+        let t2 = Instant::now();
+        t2 - t1
+    }
+    pub fn ping(&mut self) -> Duration {
+        let t1 = Instant::now();
+        self.send(b"ping", b"ping").unwrap();
+        self.receive().unwrap();
+        let t2 = Instant::now();
+        t2 - t1
     }
 }
 
 fn main() {
     let mut server = Server::new("127.0.0.1", "3333").unwrap();
-    loop {
+    // get handshake information
+    server.handshake();
+    // header length
+    // commands
+    server.receive().unwrap();
+    println!("ping {}ms", server.ping().as_millis());
+    /* loop {
         let mut msg = String::new();
         println!("Message: ");
         let _ = std::io::stdin().read_line(&mut msg).unwrap();
         let msg = msg.strip_suffix("\n").unwrap().as_bytes();
         server.send(msg).unwrap();
         println!("{}", from_utf8(&server.receive().unwrap()).unwrap());
-    }
+    } */
     println!("Terminated.");
 }
 
@@ -73,4 +167,8 @@ fn pad_back(msg: &str, len: usize, ch: &str) -> String {
         out += ch;
     }
     out
+}
+
+fn pop(barry: &[u8]) -> [u8; 8] {
+    barry.try_into().expect("slice with incorrect length")
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
 use std::io::Error;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
@@ -8,14 +10,20 @@ pub struct Server {
     listener: TcpListener,
     header_size: usize,
     port: String,
+    packet_types: HashMap<String, String>,
 }
 impl Server {
     pub fn new(addr: &str, port: &str) -> Server {
         let listener = TcpListener::bind(String::from(addr) + ":" + port).unwrap();
+        let mut map = HashMap::new();
+        map.insert(String::from("request"), String::from("rqst"));
+        map.insert(String::from("data"), String::from("data"));
+        map.insert(String::from("ping"), String::from("ping"));
         Server {
             listener,
             header_size: 8,
             port: String::from(port),
+            packet_types: map,
         }
     }
     pub fn listen(&mut self) {
@@ -50,7 +58,7 @@ impl Client {
             header_size: 8,
         }
     }
-    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+    pub fn receive_basic(&mut self) -> Result<Vec<u8>, Error> {
         let mut head = vec![0 as u8; self.header_size];
         match self.stream.read(&mut head) {
             Ok(_) => {
@@ -64,29 +72,103 @@ impl Client {
             Err(e) => Err(e),
         }
     }
-    pub fn send(&mut self, msg: &[u8]) -> Result<usize, Error> {
+    pub fn receive(&mut self) -> Result<Vec<u8>, Error> {
+        let head_structure = self.head_structure();
+        let mut header_size: usize = 0;
+        for i in head_structure {
+            header_size += i.1;
+        }
+        let mut data = vec![0 as u8; header_size];
+        match self.stream.read(&mut data) {
+            Ok(_) => {
+                let mut head: HashMap<String, &[u8]> = HashMap::new();
+                let mut current: usize = 0;
+                for i in self.head_structure() {
+                    head.insert(String::from(i.0), &data[current..current + i.1]);
+                    current += i.1
+                }
+                let len = head.get("len").copied().unwrap();
+                let len = usize::from_le_bytes(pop(len));
+                let pkg_type = from_utf8(head.get("pkg_type").unwrap()).unwrap();
+                let mut data = vec![0 as u8; len];
+                match self.stream.read_exact(&mut data) {
+                    Ok(_) => {
+                        match pkg_type {
+                            "data" => {
+                                println!("{}", from_utf8(&data).unwrap())
+                            }
+                            "ping" => {
+                                self.send(b"ping", b"ping").unwrap();
+                                println!("Client: {} pinged", self.stream.peer_addr().unwrap())
+                            }
+                            &_ => {}
+                        }
+                        Ok(data)
+                    }
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn send_basic(&mut self, msg: &[u8]) -> Result<usize, Error> {
         self.stream
             .write(pad_front(&msg.len().to_string(), HEADER_SIZE, "0").as_bytes())
             .unwrap();
         self.stream.write(msg)
     }
+    pub fn send(&mut self, pkg_type: &[u8], msg: &[u8]) -> Result<usize, Error> {
+        let len = msg.len().to_le_bytes();
+        let mut head = Vec::from(len);
+        head.extend_from_slice(pkg_type);
+        // println!("{:?}", head);
+        self.stream.write(&head).unwrap();
+        self.stream.write(msg)
+    }
+    pub fn handshake_data(&mut self) -> Vec<u8> {
+        let len_length: usize = 8;
+        let pkg_type_length = "ping".as_bytes().len();
+        let head_structure = [("len", len_length), ("pkg_type", pkg_type_length)];
+        let mut string = String::new();
+        for tuple in head_structure {
+            string += &format!("{}:{},", tuple.0, tuple.1);
+        }
+        string = String::from(&string[..string.len() - 1]); // remove trailing ,
+                                                            // println!("{}", string);
+        string.into_bytes()
+    }
+    pub fn head_structure(&mut self) -> Vec<(&str, usize)> {
+        let len_length: usize = 8;
+        let pkg_type_length = "ping".as_bytes().len();
+        vec![("len", len_length), ("pkg_type", pkg_type_length)]
+    }
+    pub fn handshake(&mut self) {
+        /* let num = (51234 as u32).to_le_bytes();
+        self.send_basic(&num).unwrap(); */
+        let ping = self.receive_basic().unwrap();
+        self.send_basic(&ping).unwrap();
+        let i = self.handshake_data();
+        self.send_basic(&i).unwrap();
+        self.send(b"rqst", b"name").unwrap(); // change
+        let name = self.receive().unwrap();
+        // println!("name: {}", from_utf8(&name).unwrap());
+    }
 }
 
 const HEADER_SIZE: usize = 8;
+// const HEAD: Vec<(String, usize)> = vec![(String::from("len"), 4), (String::from("cmd"), 4)];
 
 fn handle_client(stream: TcpStream) {
     let mut client = Client::new(stream);
-    while match client.receive() {
-        Ok(data) => {
-            client.send(&data).unwrap();
-            true
-        }
-        Err(e) => false,
-    } {}
+    client.handshake();
+
+    loop {
+        client.receive().unwrap();
+    }
 }
 
 fn main() {
-    let mut server = Server::new("127.0.0.1", "3333");
+    let mut server = Server::new("0.0.0.0", "3333");
     server.listen();
 }
 
@@ -103,4 +185,8 @@ fn pad_back(msg: &str, len: usize, ch: &str) -> String {
         out += ch;
     }
     out
+}
+
+fn pop(barry: &[u8]) -> [u8; 8] {
+    barry.try_into().expect("slice with incorrect length")
 }
